7e3dea04f24a1671838338fa0f0525df69fc0ab4
Taifinkaba <taifinkabad@gmail.com>
2024-06-06 11:04:12 -0400

    did the feedback

--- src/modify.js
+++ src/modify.js
@@ -52,20 +52,28 @@ const removeOldInfo = () => {
 removeOldInfo();
 /** FEEDBACK: You are really close! Instead of using fromCharSet, you could also initialize a string with the alphabet and use that as your indexes! */
 const makeAlphabet = () => {
-  const letters = document.getElementById("alphabet");
-  const num = parseInt(letters.dataset.numLetters);
-  for (let i = 0; i < num; i++) {
-    const item = document.createElement("li");
-    const character = String.fromCharCode(65 + i);
-    item.textContent = `${character} is letter #${i + 1} in the alphabet`;
-    letters.appendChild(item);
-  } 
+  const alpha = document.querySelector("#alphabet")
+  const count = alpha.dataset.numLetters
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  for(let i = 0; i < count; i++){
+    const list = document.createElement("li")
+    list.textContent = `${alphabet[i]} is letter #${i + 1} in the alphabet`
+    document.querySelector("#alphabet").appendChild(list)
+  }
 };
 makeAlphabet();
 
 /** FEEDBACK: Attempt this question! It basically requires you to use .innerHTML! And make sure that the HTML matches the one given, including the spacing and indentation! */
 const makeBio = () => {
-
+  document.getElementById('my-bio').innerHTML = `
+  <h2 id="bio-heading">About Me</h2>
+  <p>My name is Zo and I like learn cool new things</p>
+  <h3 id="hobby-heading">My Hobbies</h3>
+  <ul>
+    <li>Running</li>
+    <li>Reading</li>
+    <li>Writing</li>
+  </ul>`;
 };
 
 // "runner" function
